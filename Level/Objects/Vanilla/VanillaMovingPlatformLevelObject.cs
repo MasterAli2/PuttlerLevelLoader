4274010ed832965d1d44a8a7604fd816da490889
MasterAli2 <88894545+MasterAli2@users.noreply.github.com>
2026-07-11 20:47:06 -0400

    fix: fixed tool movement  for moving platform

--- Level/Objects/Vanilla/VanillaMovingPlatformLevelObject.cs
+++ Level/Objects/Vanilla/VanillaMovingPlatformLevelObject.cs
@@ -1,12 +1,18 @@
 using System;
 using Il2Cpp;
+using MelonLoader;
 using UnityEngine;
+using UnityEngine.UI;
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
 
 [RegisterLevelObject("moving platform")]
 [MelonLoader.RegisterTypeInIl2Cpp]
-public class VanillaMovingPlatformLevelObject : BaseLevelObject, IBaseLevelObject
+public class VanillaMovingPlatformLevelObject : LinkedLevelObject, IBaseLevelObject
 {
+    public bool isShadow = false;
+    public Transform pointA;
+    public Transform pointB;
+    public Transform shadow;
     public MovingPlatform movingPlatform;
     public MovingShadowPlatform movingShadowPlatform;
 
@@ -14,22 +20,49 @@ public class VanillaMovingPlatformLevelObject : BaseLevelObject, IBaseLevelObjec
 
     public VanillaMovingPlatformLevelObject(IntPtr ptr) : base(ptr) {}
 
-    public new void Awake()
-    {
-        base.Awake();
 
-        movingPlatform = GetComponent<MovingPlatform>();
-        movingShadowPlatform = GetComponent<MovingShadowPlatform>();
-    }
 
     public static GameObject Place(SerialLevelObject serialLevelObject)
     {
-        GameObject obj = placeMovingPlatform(
+        (Transform obj1, Transform obj2, Transform pointB, Transform pointA, Transform myShadowObject) = placeMovingPlatform(
             Vec3D.fromJson(serialLevelObject.data["start"]),
             Vec3D.fromJson(serialLevelObject.data["end"]),
             serialLevelObject.data["rot"].GetSingle());
-        obj.AddComponent<VanillaMovingPlatformLevelObject>();
-        return obj;
+
+        VanillaMovingPlatformLevelObject platform = obj1.gameObject.AddComponent<VanillaMovingPlatformLevelObject>();
+        platform.isShadow = false;
+
+        
+        //VanillaMovingPlatformLevelObject shadow = obj2.gameObject.AddComponent<VanillaMovingPlatformLevelObject>();
+        //VanillaMovingPlatformLevelObject shadow = pointB.gameObject.AddComponent<VanillaMovingPlatformLevelObject>();
+        VanillaMovingPlatformLevelObject shadow = myShadowObject.gameObject.AddComponent<VanillaMovingPlatformLevelObject>();
+        shadow.isShadow = true;
+        
+        platform.movingPlatform = obj1.gameObject.GetComponent<MovingPlatform>();
+        platform.movingShadowPlatform = obj1.gameObject.GetComponent<MovingShadowPlatform>();
+        shadow.movingPlatform = obj1.gameObject.GetComponent<MovingPlatform>();
+        shadow.movingShadowPlatform = obj1.gameObject.GetComponent<MovingShadowPlatform>();
+
+        platform.links.Add(shadow);
+        shadow.links.Add(platform);
+        platform.links.Add(pointA.gameObject);
+        shadow.links.Add(pointA.gameObject);
+        platform.links.Add(pointB.gameObject);
+        shadow.links.Add(pointB.gameObject);
+        platform.links.Add(obj1.GetComponent<MovingShadowPlatform>().platform.gameObject);
+        shadow.links.Add(obj1.GetComponent<MovingShadowPlatform>().platform.gameObject);
+
+        platform.pointA = pointA;
+        platform.pointB = pointB;
+        platform.shadow = obj2.transform;
+
+        shadow.pointA = pointA;
+        shadow.pointB = pointB;
+        shadow.shadow = obj2.transform;
+
+
+
+        return platform.gameObject;
     }
 
     public static void CleanScene()
@@ -38,26 +71,132 @@ public class VanillaMovingPlatformLevelObject : BaseLevelObject, IBaseLevelObjec
         Utils.HideAndDisable(prefab);
     }
 
-    public static GameObject placeMovingPlatform(Vector3 start, Vector3 end, float rotation)
+    public static (Transform, Transform, Transform, Transform, Transform) placeMovingPlatform(Vector3 start, Vector3 end, float rotation)
     {
-        GameObject obj = Utils.spawnPrefab(prefab);
-
-        obj.transform.position = start;
-        obj.transform.eulerAngles = new Vector3(0f, 0f, rotation);
+        Transform obj = Utils.spawnPrefab(prefab).transform;
 
 
-        obj.transform.GetChild(3).position = end;
 
-        return obj;
+        Transform movingPlatform = obj.GetChild(0);
+        MovingPlatform originalMovingPlatform = obj.GetComponent<MovingPlatform>();
+        MovingPlatform newMovingPlatform = movingPlatform.gameObject.AddComponent<MovingPlatform>();
+
+        newMovingPlatform.platform = movingPlatform;
+
+        newMovingPlatform.pointA = originalMovingPlatform.pointA;
+        newMovingPlatform.pointB = originalMovingPlatform.pointB;
+
+        newMovingPlatform.speed = originalMovingPlatform.speed;
+        newMovingPlatform.startGoingPointA = originalMovingPlatform.startGoingPointA;
+
+        Destroy(originalMovingPlatform);
+
+
+        Transform shadowMovingPlatform = obj.GetChild(1);
+        MovingShadowPlatform originalShadowMovingPlatform = obj.GetComponent<MovingShadowPlatform>();
+        MovingShadowPlatform newShadowMovingPlatform = movingPlatform.gameObject.AddComponent<MovingShadowPlatform>();
+
+        newShadowMovingPlatform.platform = shadowMovingPlatform;
+        newShadowMovingPlatform.Awake();
+
+
+        
+
+        Destroy(originalShadowMovingPlatform);
+
+        Transform pointB = obj.transform.GetChild(3);
+        Transform pointA = obj.transform.GetChild(2);
+
+        pointB.position = end;
+        pointA.position = start;
+
+
+        SpriteRenderer shadowObjectRenderer = (new GameObject("Shadow Level Object")).AddComponent<SpriteRenderer>();
+        shadowObjectRenderer.sprite = shadowMovingPlatform.gameObject.GetComponent<SpriteRenderer>().sprite;
+        Color shadowColor = shadowMovingPlatform.gameObject.GetComponent<SpriteRenderer>().color;
+        shadowColor.a = 157.5f / 255f;
+        shadowObjectRenderer.color = shadowColor;
+
+        BoxCollider2D shadowCollider = shadowObjectRenderer.gameObject.AddComponent<BoxCollider2D>();
+        shadowCollider.isTrigger = true;
+        shadowCollider.sharedMaterial = movingPlatform.gameObject.GetComponent<BoxCollider2D>().sharedMaterial;
+
+        shadowCollider.transform.position = pointB.transform.position;
+        shadowCollider.transform.localScale = new Vector3(2f, 0.2f, 1f);
+
+
+        obj.DetachChildren();
+        Destroy(obj.gameObject);
+
+
+
+
+
+
+        movingPlatform.position = start;
+        movingPlatform.eulerAngles = new Vector3(0f, 0f, rotation);
+
+        shadowMovingPlatform.position = start;
+        shadowMovingPlatform.eulerAngles = new Vector3(0f, 0f, rotation);
+
+
+
+        return (movingPlatform, shadowMovingPlatform, pointB, pointA, shadowCollider.transform);
+    }
+
+    public override void OnDestroy()
+    {
+        base.OnDestroy();
+
+        if (isShadow)
+        {
+            //Destroy(pointB.gameObject);
+        }
+        else
+        {   
+            //Destroy(GetComponent<MovingShadowPlatform>().platform.gameObject);
+            //Destroy(pointA.gameObject);
+        }
     }
 
     public override void OnEditorPickup()
     {
         // nothing
     }
+
     public override void OnEditorDrop()
     {
-        movingPlatform.Start();
-        movingShadowPlatform.Start();
+
+        var a = movingShadowPlatform.targetPosition;
+        
+        if (isShadow)
+        {
+            if (a == pointB.position)
+            {
+                a = transform.position;
+            }
+
+            pointB.position = transform.position; 
+        }
+        else
+        {
+            if (a == pointA.position)
+            {
+                a = transform.position;
+            }
+            pointA.position = transform.position;
+        }
+
+
+        // TODO: fix startGoingA field thing
+        //movingPlatform.Start();
+        movingPlatform.startPosition = pointA.position;
+        movingPlatform.targetPosition = pointB.position;
+
+        //movingShadowPlatform.Start();
+        movingShadowPlatform.startPosition = pointA.position;
+        movingShadowPlatform.targetPosition = a;
+
     }
 }
+
