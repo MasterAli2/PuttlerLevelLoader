6ed711dd17bb5c6b634d4336ca25e5f643c32232
MasterAli2 <88894545+MasterAli2@users.noreply.github.com>
2026-07-13 18:27:51 -0400

    add an extra temp registry

--- Level/Objects/IBaseLevelObject.cs
+++ Level/Objects/IBaseLevelObject.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public interface IBaseLevelObject
 {
     public abstract static void CleanScene();
-    public abstract static GameObject Place(SerialLevelObject serialLevelObject);
+    public abstract static GameObject[] Place(SerialLevelObject serialLevelObject);
+    public abstract static GameObject[] PlaceDefault();
+    public abstract static void ApplyEditorPlaceButtons(GameObject gameObject);
 
 }
--- Level/Objects/LevelObjectRegistry.cs
+++ Level/Objects/LevelObjectRegistry.cs
@@ -9,13 +9,17 @@ using UnityEngine;
 // this is mostly AI generated but it works so whatever
 public static class LevelObjectRegistry
 {
-    public static Dictionary<string, Func<SerialLevelObject, GameObject>> Registry;
+    public static Dictionary<string, Func<SerialLevelObject, GameObject[]>> Registry;
+    public static Dictionary<string, Func<GameObject[]>> PlaceDefaultRegistry;
     public static Dictionary<string, Action> CleanupRegistry;
+    public static Dictionary<string, Action<GameObject>> EditorPlaceButtonRegistry;
 
     public static void Build()
     {
-        Registry = new Dictionary<string, Func<SerialLevelObject, GameObject>>();
+        Registry = new Dictionary<string, Func<SerialLevelObject, GameObject[]>>();
+        PlaceDefaultRegistry = new Dictionary<string, Func<GameObject[]>>();
         CleanupRegistry = new Dictionary<string, Action>();
+        EditorPlaceButtonRegistry = new Dictionary<string, Action<GameObject>>();
 
         foreach (var type in Assembly.GetExecutingAssembly().GetTypes()
             .Where(t =>
@@ -31,15 +35,30 @@ public static class LevelObjectRegistry
                 nameof(IBaseLevelObject.Place),
                 BindingFlags.Public | BindingFlags.Static);
 
+            var placeDefaultMethod = type.GetMethod(
+                nameof(IBaseLevelObject.PlaceDefault),
+                BindingFlags.Public | BindingFlags.Static);
+
             var cleanMethod = type.GetMethod(
                 nameof(IBaseLevelObject.CleanScene),
                 BindingFlags.Public | BindingFlags.Static);
 
+            var applyEditorPlaceButtonsMethod = type.GetMethod(
+                nameof(IBaseLevelObject.ApplyEditorPlaceButtons),
+                BindingFlags.Public | BindingFlags.Static);
+
             Registry[attribute.Name] = serial =>
-                (GameObject)placeMethod!.Invoke(null, new object[] { serial })!;
+                (GameObject[])placeMethod!.Invoke(null, new object[] { serial })!;
+                
+            PlaceDefaultRegistry[attribute.Name] = () =>
+                (GameObject[])placeDefaultMethod!.Invoke(null, null)!;
 
             CleanupRegistry[attribute.Name] = () =>
                 cleanMethod!.Invoke(null, null);
+
+            EditorPlaceButtonRegistry[attribute.Name] = gameObject =>
+                applyEditorPlaceButtonsMethod?.Invoke(null, new object[] { gameObject });
+            
         }
     }
 }
