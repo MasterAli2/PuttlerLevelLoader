e5eb63d7f1dbfeba8c33cce01d5737cfd9a64eca
MasterAli2 <88894545+MasterAli2@users.noreply.github.com>
2026-07-13 18:25:44 -0400

    Add runtime sprite helper

--- Utils.cs
+++ Utils.cs
@@ -90,5 +90,76 @@ public static class Utils
         collider.contactCaptureLayers = 0;
         collider.callbackLayers = 0;
     }
+
+    public static class RuntimeSprite
+    {
+        public const int BASE_TEX_SIZE = 256;
+        public static Sprite _square;
+        public static Sprite square {get
+            {
+                if (_square != null) return _square;
+
+                Texture2D tex = Texture2D.whiteTexture;
+                _square = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2(0.5f, 0.5f), tex.width);
+
+                return _square;
+            }
+        }
+        public static Sprite _circle;
+        public static Sprite circle {get
+            {
+                if (_circle != null) return _circle;
+
+                _circle = getCircleSprite();
+                return _circle;
+            }
+        }
+
+        public static Sprite getCircleSprite()
+        {
+            Texture2D texture = new Texture2D(BASE_TEX_SIZE, BASE_TEX_SIZE, TextureFormat.RGBA32, false);
+
+            texture.wrapMode = TextureWrapMode.Clamp;
+            texture.filterMode = FilterMode.Bilinear;
+
+            float center = BASE_TEX_SIZE / 2f;
+            float radius = BASE_TEX_SIZE / 2f;
+            
+            float radiusSquared = radius * radius; 
+
+            for (int y = 0; y < BASE_TEX_SIZE; y++)
+            {
+                for (int x = 0; x < BASE_TEX_SIZE; x++)
+                {
+                    float dx = (x + 0.5f) - center;
+                    float dy = (y + 0.5f) - center;
+
+                    float distanceSquared = (dx * dx) + (dy * dy);
+
+                    if (distanceSquared <= radiusSquared)
+                    {
+                        texture.SetPixel(x, y, Color.white);
+                    }
+                    else
+                    {
+                        texture.SetPixel(x, y, Color.clear);
+                    }
+                }
+            }
+
+            texture.Apply();
+
+            Sprite circleSprite = Sprite.Create(
+                texture, 
+                new Rect(0, 0, texture.width, texture.height), 
+                new Vector2(0.5f, 0.5f), 
+                texture.width                     
+            );
+
+            return circleSprite;
+        }
+
+
+    }
     
 }
