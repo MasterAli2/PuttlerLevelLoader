ca2a4abf2851e01bc27872a95733c0d7499f13ae
MasterAli2 <88894545+MasterAli2@users.noreply.github.com>
2026-07-13 13:59:05 -0400

    Add editor camera zoom

--- Editor/EditorCamera.cs
+++ Editor/EditorCamera.cs
@@ -12,6 +12,14 @@ class EditorCamera : MonoBehaviour
 
     public CameraScroll cameraScroll;
 
+
+    public float zoomSensitivity = 0.25f;
+    public float minZoom = 2.5f;
+    public float maxZoom = 10f;
+
+    public readonly float maxHorizontalDistance = 24f;
+    public readonly float maxVerticalDistance = 24f;
+
     public Camera cam
     {
         get
@@ -23,12 +31,31 @@ class EditorCamera : MonoBehaviour
     void Awake()
     {
         cameraScroll = cam.GetComponent<CameraScroll>();
-        cameraScroll.leftDistance = 24f;
-        cameraScroll.rightDistance = 24f;
+        cameraScroll.leftDistance = maxHorizontalDistance;
+        cameraScroll.rightDistance = maxHorizontalDistance;
     }
 
     void Update()
     {
+        float scroll = -Input.mouseScrollDelta.y;
+        if (scroll != 0)
+        {
+            var a = cam.ScreenToWorldPoint(Input.mousePosition);
+
+            cam.orthographicSize = Mathf.Clamp(cam.orthographicSize + scroll*zoomSensitivity, minZoom, maxZoom);
+
+            var b = cam.ScreenToWorldPoint(Input.mousePosition);
+
+            MelonLogger.Msg(a==b);
+
+            Vector2 difference = a - b;
+            Vector3 newPosition = cam.transform.position + (Vector3)difference;
+            newPosition.y = Mathf.Clamp(newPosition.y, -maxVerticalDistance, maxVerticalDistance);
+            cam.transform.position = newPosition;
+            cameraScroll.targetPosition = cam.transform.position.x;
+        }
+
+
         if (Input.GetMouseButtonDown(1))
         {
             dragOrigin = cam.ScreenToWorldPoint(Input.mousePosition);
@@ -41,7 +68,7 @@ class EditorCamera : MonoBehaviour
             Vector2 difference = dragOrigin - current;
 
             Vector3 newPosition = cam.transform.position + (Vector3)difference;
-            newPosition.y = Mathf.Clamp(newPosition.y, -12f, 12f);
+            newPosition.y = Mathf.Clamp(newPosition.y, -maxVerticalDistance, maxVerticalDistance);
             cam.transform.position = newPosition;
             cameraScroll.targetPosition = cam.transform.position.x;
         }
